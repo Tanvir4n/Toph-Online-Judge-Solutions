bd29f1f97266315711d3445356542f3e4645e948
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2023-11-29 12:41:14 +0600

    Create Is Anagram.cpp

---
+++ Easy Problems/Is Anagram.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string str1,str2;
+    cin>>str1>>str2;
+    sort(str1.begin(),str1.end());
+    sort(str2.begin(),str2.end());
+    if(str1 == str2)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+	return 0;
+}
