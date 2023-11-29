435a01fa575db7d7c64d45c895d87d23d04bc0d4
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2023-11-29 12:50:21 +0600

    Create The Third Problem.cpp

---
+++ Easy Problems/The Third Problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int num[10];
+    int i=0;
+    while(cin>>num[i]){
+        i++;
+    }
+
+    cout<<num[2]<<endl;
+
+    return 0;
+}
